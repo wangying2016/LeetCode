a5a9399477e0c9f8d2f7e6d1a08044cd83ffd3a7
wang ying <wy867504146@outlook.com>
2017-05-24 21:06:24 +0800

    453. Minimum Moves to Equal Array Elements

--- 453. Minimum Moves to Equal Array Elements/Minimum Moves to Equal Array Elements.cpp
+++ 453. Minimum Moves to Equal Array Elements/Minimum Moves to Equal Array Elements.cpp
@@ -11,19 +11,18 @@ using std::max_element;
 using std::min_element;
 using std::distance;
 
-// my solution 1 , runtime error , error case [1,2147483647]
+// my solution 1 , Compile Error
 class Solution1 {
 public:
 	int minMoves(vector<int>& nums) {
-		auto maxIt = max_element(nums.begin(), nums.end());
+		auto min = min_element(nums.begin(), nums.end());
+		auto max = max_element(nums.begin(), nums.end());
+		if (*min == *max) return count;
 		for (int i = 0; i < nums.size(); ++i) {
-			if (i != distance(nums.begin(), maxIt)) ++nums[i];
+			if (max - nums.begin() != i) ++nums[i];
 		}
-		maxIt = max_element(nums.begin(), nums.end());
-		auto minIt = min_element(nums.begin(), nums.end());
 		++count;
-		if (*maxIt == *minIt) return count;
-		else return minMoves(nums);
+		return minMoves(nums);
 	}
 
 private:
@@ -53,11 +52,57 @@ private:
 	int count = 0;
 };
 
+// my solution 3 , runtime error , error case 
+// [83,-14,77,15,93,35,86,-8,-51,-79,62,-73,-10,-41,63,26,40,-74,72,36,-89,68,67,-71,82,30,-38,23,-33,35,29,-98,-78,-42...
+class Solution3 {
+public:
+	int minMoves(vector<int>& nums) {
+		auto max = max_element(nums.begin(), nums.end());
+		auto min = min_element(nums.begin(), nums.end());
+		if (*max == *min) return count;
+		int maxTemp = *max;
+		*max = INT_MIN;
+		auto max_second = max_element(nums.begin(), nums.end());
+		*max = maxTemp;
+		int distance = *max - *max_second == 0 ? 1 : *max - *max_second;
+		count += distance;
+		for (int i = 0; i < nums.size(); ++i) {
+			if (max - nums.begin() != i) nums[i] += distance;
+		}
+		return minMoves(nums);
+	}
+
+private:
+	int count = 0;
+};
+
+// perfect solution , runtime = 52 ms
+class Solution4 {
+public:
+	int minMoves(vector<int>& nums) {
+		int sum = 0, moves = 0;
+		auto minIt = min_element(nums.begin(), nums.end());
+		for (auto i : nums) sum += i;
+		return sum - *minIt * nums.size();
+	}
+};
+
+// another perfect solution , runtime = 53 ms
+class Solution5 {
+public:
+	int minMoves(vector<int>& nums) {
+		int moves = 0;
+		auto minIt = min_element(nums.begin(), nums.end());
+		for (auto i : nums) moves += i - *minIt;
+		return moves;
+	}
+};
+
+
 int main()
 {
-	/*vector<int> nums = { 1, 2, 3 };*/
-	vector<int> nums = { -2147483648, -2147483647 };
-	Solution2 solution;
+	vector<int> nums = { 1, 2, 3 };
+	Solution5 solution;
 	cout << solution.minMoves(nums) << endl;
 	system("pause");
 	return 0;
