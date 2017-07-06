adb4b1edd3127eba662f61c0785cb2ab3261c3fa
wang ying <wy867504146@outlook.com>
2017-07-06 17:56:27 +0800

    551. Student Attendance Record I

---
+++ 551. Student Attendance Record I/Student Attendance Record I.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <stack>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::stack;
+
+// my first soluton , runtime = 6 ms 
+class Solution1 {
+public:
+	bool checkRecord(string s) {
+		stack<char> a, l;
+		for (auto i : s) {
+			if (i == 'A') {
+				if (a.size() == 1) return false;
+				else a.push(i);
+			}
+			if (i == 'L') {
+				if (l.size() == 2) return false;
+				else l.push(i);
+			}
+			else while (l.size()) l.pop();
+		}
+		return true;
+	}
+};
+
+// my solution 2 , runtime = 3 ms
+class Solution2 {
+public:
+	bool checkRecord(string s) {
+		bool hasA = false;
+		int countL = 0;
+		for (auto i : s) {
+			if (i == 'A') {
+				if (hasA) return false;
+				else hasA = true;
+			}
+			if (i == 'L') {
+				if (countL == 2) return false;
+				else countL++;
+			}
+			else countL = 0;
+		}
+		return true;
+	}
+};
+
+// my solution 3 , runtime = 3 ms
+class Solution3 {
+public:
+	bool checkRecord(string s) {
+		int a = 0, l = 0;
+		for (auto i : s) {
+			if (i == 'A') a++;
+			if (i == 'L') l++;
+			else l = 0;
+			if (a > 1 || l > 2) return false;
+		}
+		return true;
+	}
+};
+
+int main()
+{
+	Solution1 solution;
+	string s = "LLPLL";
+	cout << solution.checkRecord(s) << endl;
+	system("pause");
+	return 0;
+}
+
